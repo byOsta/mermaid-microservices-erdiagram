d6a343b6c21ffbbc706ed5042e94fc3864d118f3
byOsta <jorgeosta04@gmail.com>
2023-06-26 21:08:19 +0200

    Update SqlServerGenerate.sql

--- SqlServerGenerate.sql
+++ SqlServerGenerate.sql
@@ -1,16 +1,19 @@
+SET NOCOUNT ON;
+
 SELECT 'erDiagram'
 UNION ALL
 SELECT 
-    CHAR(10) + c.TABLE_NAME + ' {' +
+    '    ' + c.TABLE_NAME + ' {' +
     (
-        SELECT '\n' + CHAR(10) + CHAR(10) + DATA_TYPE  + ' ' +  COLUMN_NAME+ CHAR(10)
+        SELECT CHAR(10) + '    ' + '    ' + DATA_TYPE  + IIF(DATA_TYPE = 'nvarchar', IIF(CHARACTER_MAXIMUM_LENGTH IS NULL,  '', IIF(CHARACTER_MAXIMUM_LENGTH = -1, '(MAX)', '('  + CONVERT(varchar(20), CHARACTER_MAXIMUM_LENGTH)  + ')') ), '') + ' ' +  COLUMN_NAME
         FROM INFORMATION_SCHEMA.COLUMNS
         WHERE TABLE_SCHEMA = c.TABLE_SCHEMA
             AND TABLE_NAME = c.TABLE_NAME
+            AND COLUMN_NAME NOT LIKE 'Period%'
         ORDER BY ORDINAL_POSITION
         FOR XML PATH('')
     ) +
-    '\n' + CHAR(10) + '}'
+    CHAR(10) + '    ' + '}' + CHAR(10) 
 FROM INFORMATION_SCHEMA.TABLES c
 WHERE c.TABLE_TYPE IN ('BASE TABLE', 'VIEW')
     AND NOT EXISTS (
@@ -21,10 +24,12 @@ WHERE c.TABLE_TYPE IN ('BASE TABLE', 'VIEW')
     )
     AND c.TABLE_SCHEMA NOT LIKE 'pg_%'
     AND c.TABLE_SCHEMA <> 'information_schema'
-	AND c.TABLE_NAME NOT LIKE '%History' --TODO Remove if needed.
+	-- AND c.TABLE_NAME NOT LIKE '%History' --TODO Remove if needed.
+	AND c.TABLE_NAME NOT LIKE 'sysdiagrams' --TODO Remove if needed.
 UNION ALL
 SELECT 
-    c1.name + ' }|--|| ' + c2.name + ' : "' + f.name  + '"' + CHAR(10) 
+    -- '    ' + c1.name + ' }|--|| ' + c2.name + ' : "has"'
+    '    ' + c1.name + ' }|--|| ' + c2.name + ' : ""'
 FROM sys.foreign_keys f
 JOIN sys.tables c1 ON f.parent_object_id = c1.object_id
 JOIN sys.tables c2 ON f.referenced_object_id = c2.object_id;
